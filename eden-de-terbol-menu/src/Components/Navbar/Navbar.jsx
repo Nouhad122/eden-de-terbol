334cfacd5d116259656e89f574b61c27f289bf44
Nouhad122 <nouhadalhallab122@gmail.com>
2025-07-23 16:59:59 +0300

    fixed image issues

--- eden-de-terbol-menu/src/Components/Navbar/Navbar.jsx
+++ eden-de-terbol-menu/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Navbar.module.css'
 import logo from '../../assets/eden-logo.png'
+
 const Navbar = () => {
   return (
-    <header>
-      <img src={logo} alt="Eden de Terbol" />
+    <header className={styles.header}>
+      <img src={logo} alt="Eden de Terbol" className={styles.logo} />
     </header>
   )
 }
