79aa4e097377b2a31a5156312fb601b90bd0fbf5
Nouhad122 <nouhadalhallab122@gmail.com>
2025-09-02 01:36:45 +0300

    added images for each item, updated the menu

--- eden-de-terbol-menu/src/Components/MenuComps/MenuItem.jsx
+++ eden-de-terbol-menu/src/Components/MenuComps/MenuItem.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import styles from '../../Pages/Menu/Menu.module.css'
-
+import SaladImg from '../../assets/salad.jpg'
+import { Image } from 'antd'
 const MenuItem = ({ item }) => {
   return (
         <div className={styles.menuItem}>
             <div className={styles.itemHeader}>
-                <h3 className={styles.itemName}>{item.itemName}</h3>
+                <div className={styles.nameImageContainer}>
+                {/* {
+                    item.imageId && (
+                        <Image
+                        width={60}
+                        src={SaladImg}
+                        alt={item.itemName}
+                        preview={{
+                            mask: null,
+                            movable: false,
+                            zoom: false,
+                            rotatable: false,
+                            scalable: false,
+                            toolbarRender: () => null,
+                        }}
+                        style={{ cursor: 'pointer'  }}
+                        />
+                    )
+                } */}
+                    <h3 className={styles.itemName}>{item.itemName}</h3>
+                </div>
                 <span className={styles.price}>{item.price}</span>
             </div>
             <p className={styles.itemDescription}>
